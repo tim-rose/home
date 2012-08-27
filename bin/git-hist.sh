5b654be5776aab9d189e21dcdd326f5142ebad24
Tim Rose <tim@envato.com>
2012-08-27 15:18:12 +1000

    added option processing, date formatting

--- bin/git-hist.sh
+++ bin/git-hist.sh
@@ -2,5 +2,30 @@
 #
 # GIT-HIST --Customised version of git log that shows a colourful graph.
 #
-git log --graph \
-      --pretty='format:%C(bold)%h%d%Creset %ar %Cgreen%an%Creset %s'  "$@";
+PATH=$PATH:/usr/libexec:/usr/local/libexec
+. getopt.shl
+. log.shl
+
+#
+# usage() --echo this script's usage message.
+#
+usage()
+{
+    cat <<EOF
+git-hist: show the recent history of changes in this repository
+EOF
+    getopt_usage "git-hist [options] <git-log arguments>" "$1"
+}
+
+opts="r.relative_date;$LOG_GETOPTS"
+eval $(getopt_long_args -d "$opts" "$@" || usage "$opts" >&2)
+log_getopts
+
+if [ "$relative_date" ]; then
+    date_fmt='%ar'
+else
+    date_fmt='%aD'
+fi
+
+log_format="%C(bold)%h%C(cyan)%d%Creset $date_fmt %Cgreen%an%Creset %s"
+git log --graph --pretty="format:$log_format" "$@"
