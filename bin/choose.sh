957b28ad3d6f2246d06c0ed5cc9e7e6475e90d36
Tim Rose <tim.rose@acm.org>
2019-07-19 11:50:56 +1000

    wip

--- bin/choose.sh
+++ bin/choose.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
 #
-# CHOOSE --select from a list of names
+# CHOOSE --Select from a list of names.
+#
+# Remarks:
+# Doesn't handle files containing spaces.
 #
 help='Please type y(=yes) n(=no) a(=all remaining) q(=quit, none selected)'
 newlist=""
-ask=1
-allfiles=$*
-nfiles=$(echo $allfiles|wc -w)
-for file in $allfiles
+confirm='y'
+for file in "$@"
 do
-    if [ $ask -eq 1 ]; then
+    if [ "$confirm" ]; then
 	printf '\n%s? [ynaq]: ' "$file" >/dev/tty
 	read -n1 c		# bashism
         printf '\n\n'
     fi
     case $c in
-    y) newlist="$newlist $file";;
-    a) newlist="$newlist $file"; ask=0; c="y";;
+    y) newlist="$newlist $file";; # @revisit: handle space in filenames
+    a) newlist="$newlist $file"; confirm=; c='y';;
     q) exit 1;;
     \?) printf '\n%s' "$help" >/dev/tty;;
     *) ;;
