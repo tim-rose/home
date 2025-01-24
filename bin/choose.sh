0b3aaf36a62fb800ca8c45174cabaee31cff48e0
Tim Rose <tim.rose@acm.org>
2025-01-24 15:57:48 +1100

    fix: bugs.

--- bin/choose.sh
+++ bin/choose.sh
@@ -10,18 +10,20 @@ newlist=""
 confirm='y'
 for file in "$@"
 do
-    if [ "$confirm" ]; then
-	printf '\n%s? [ynaq]: ' "$file" >/dev/tty
-	read -n1 c		# bashism
-        printf '\n\n'
-    fi
-    case $c in
-    y) newlist="$newlist $file";; # @revisit: handle space in filenames
-    a) newlist="$newlist $file"; confirm=; c='y';;
-    q) exit 1;;
-    \?) printf '\n%s' "$help" >/dev/tty;;
-    *) ;;
-    esac
+    while true; do
+        if [ "$confirm" ]; then
+            printf '%s? [ynaq]: ' "$file" >/dev/tty
+            read -n1 c		# bashism
+            printf '\n'
+        fi
+        case $c in
+        y) newlist="$newlist $file"; break;;
+        n) break;;
+        a) newlist="$newlist $file"; confirm=; c='y'; break;;
+        q) exit 1;;
+        *) printf '%s\n' "$help" >/dev/tty;;
+        esac
+    done
 done
 if [ "$newlist" ]; then
     echo $newlist
